cc468ef3c2457f0f3f6b3892d04dc8f87f2a9888
Dennis Man <dman@questica.com>
2019-12-02 05:33:59 -0500

    Day 01

--- Day 1/Program.cs
+++ Day 1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Shared_Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,53 @@ namespace Day_1
 {
 	public class Program
 	{
-		static void Main(string[] args)
+		public static void Main(string[] args)
 		{
+			var input = Utilities.ReadFile(args[0]);
+
+			// Part 1
+			var sum = 0;
+
+			foreach (var mass in input)
+			{
+				sum += CalculateFuel(mass, 3, -2);
+			}
+
+			Console.WriteLine(sum);
+
+			// Part 2
+
+			sum = 0;
+			int i = 0;
+
+			foreach (var mass in input)
+			{
+				var partialFuel = 0;
+
+				var fuel = CalculateFuel(mass, 3, -2);
+				partialFuel += fuel;
+				do
+				{
+					fuel = CalculateFuel(fuel, 3, -2);
+					partialFuel += fuel;
+				} while (fuel > 6);
+
+				sum += partialFuel;
+			}
+
+			Console.WriteLine(sum);
+			Console.ReadKey();
+
+		}
+
+		public static int CalculateFuel(string input, int divisor, int adjustment)
+		{
+			return int.Parse(input) / divisor + adjustment;
+		}
+
+		public static int CalculateFuel(int input, int divisor, int adjustment)
+		{
+			return input / divisor + adjustment;
 		}
 	}
 }
---
+++ Shared Utilities/Utilities.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Shared_Utilities
+{
+
+	public static class Utilities
+	{
+		public static IList<string> ReadFile(string path)
+		{
+			var list = new List<string>();
+			var file = path;
+
+			using (var fs = new FileStream(file, FileMode.Open, FileAccess.Read))
+			{
+				using (var sr = new StreamReader(fs))
+				{
+					while (!sr.EndOfStream)
+						list.Add(sr.ReadLine());
+				}
+			}
+
+			return list;
+		}
+
+		public static string ReadFileAsString(string path)
+		{
+			using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+			{
+				using (var sr = new StreamReader(fs))
+					return sr.ReadToEnd();
+			}
+		}
+	}
+}
