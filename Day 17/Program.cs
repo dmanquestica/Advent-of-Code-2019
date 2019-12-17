c6e5212804f8c3be5006da7da2e3f3ac7eec6c79
Dennis <dmanquestica@users.noreply.github.com>
2019-12-17 15:01:39 -0500

    Day 17 not complete

--- Day 17/Program.cs
+++ Day 17/Program.cs
@@ -5,19 +5,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Day_15
+namespace Day_17
 {
 	class Coords
 	{
 		public Int64 X;
 		public Int64 Y;
 		public int Distance;
+		public Tile Tile;
 
-		public Coords(Int64 x, Int64 y, int distance)
+		public Coords(Int64 x, Int64 y, int distance, Tile tile)
 		{
 			X = x;
 			Y = y;
 			Distance = distance;
+			Tile = tile;
 		}
 	}
 
@@ -26,6 +28,16 @@ namespace Day_15
 		Positional = 0,
 		Immediate = 1,
 		Relative = 2
+	}
+
+	public enum Tile
+	{
+		Scaffold = 35,
+		Empty = 46,
+		RobotUp = 94,
+		RobotDown = 86,
+		RobotLeft = 60,
+		RobotRight = 62
 	}
 
 	class Intcode
@@ -47,7 +59,7 @@ namespace Day_15
 
 		public Intcode(Int64[] initialMemory)
 		{
-			Memory = new Int64[initialMemory.Count()];
+			Memory = new Int64[initialMemory.Count() + 10000];
 			initialMemory.CopyTo(Memory, 0);
 		}
 
@@ -264,40 +276,46 @@ namespace Day_15
 		}
 	}
 
-	class Drone
+	class ASCIIRobot
 	{
 		public Int64[] Program;
 		public Intcode Brain;
 		public Coords Coordinates;
-		public bool AtTarget;
+		public Tile Direction;
 
-		public Drone(Int64[] input, Coords coords, bool atTarget = false)
+		public ASCIIRobot(Int64[] input, Coords coords = null)
 		{
 			Program = new Int64[(Int64)input.Length];
 			input.CopyTo(Program, 0);
-			AtTarget = atTarget;
 			Coordinates = coords;
+			if (coords == null)
+				Brain = new Intcode(Program);
 		}
 
-		public Drone Move(int direction, HashSet<(Int64, Int64)> explored)
+		public ASCIIRobot Move(Tile direction, HashSet<(Int64, Int64)> explored)
 		{
 			Brain = new Intcode(Program);
+			Direction = direction;
 
-			var nextCoord = new Coords(Coordinates.X, Coordinates.Y, Coordinates.Distance + 1);
+			var nextCoord = new Coords(Coordinates.X, Coordinates.Y, Coordinates.Distance + 1, direction);
 
 			switch (direction)
 			{
-				case 1:
+				case Tile.RobotUp:
 					nextCoord.Y++;
+					direction = Tile.RobotUp;
 					break;
-				case 2:
+				case Tile.RobotDown:
 					nextCoord.Y--;
+					direction = Tile.RobotDown;
 					break;
-				case 3:
+				case Tile.RobotRight:
 					nextCoord.X++;
+					direction = Tile.RobotRight;
 					break;
-				case 4:
+				case Tile.RobotLeft:
 					nextCoord.X--;
+					direction = Tile.RobotLeft;
 					break;
 			}
 			if (explored.Contains((nextCoord.X, nextCoord.Y)))
@@ -305,95 +323,211 @@ namespace Day_15
 
 			explored.Add((nextCoord.X, nextCoord.Y));
 
-			Brain.SendInput(direction);
+			Brain.SendInput((long)direction);
 			Brain.Run();
 			var output = Brain.GetOutput();
 
-			if (output == 0) 
+			if ((Tile)output == Tile.Empty)
 				return null;
-			if (output == 2) 
-				return new Drone(Brain.Memory, nextCoord, true);
-			if (output == 1) 
-				return new Drone(Brain.Memory, nextCoord);
+			if ((Tile)output == Tile.Scaffold)
+				return new ASCIIRobot(Brain.Memory, nextCoord);
 			else throw new Exception("Unknown result");
 		}
 	}
 
+
 	class Program
 	{
-		public static HashSet<(Int64, Int64)> Explored;
-		public static Queue<Drone> Positions;
-		public static Drone onTarget;
+		public static List<Coords> Map = new List<Coords>();
 
 		static void Main(string[] args)
 		{
 			var input = Utilities.ReadFileAsString(args[0]).Split(',').Select(Int64.Parse).ToArray();
 
-			Console.WriteLine($"Part 1: {Part1(input)}");
-			Console.WriteLine($"Part 2: {Part2(input)}");
+			Int64[] realInput = new Int64[input.Length];
+			input.CopyTo(realInput, 0);
+
+			// Part 1
+			Part1(realInput);
+
+			// Part 2
+			realInput = new long[input.Length];
+			input.CopyTo(realInput, 0);
+			Part2(realInput);
+
 			Console.ReadKey();
 		}
 
-		// BFS to Oxygen System
-		public static int Part1(Int64[] input)
+		static void Part1(Int64[] input)
 		{
-			Explored = new HashSet<(Int64, Int64)>();
-			Positions = new Queue<Drone>();
+			var bot = new ASCIIRobot(input, null);
+			bot.Brain.Run();
+			var output = bot.Brain.GetOutputQueue();
 
-			var drone = new Drone(input, new Coords(0,0,0));
-			Positions.Enqueue(drone);
-
-			while (Positions.Count > 0)
+			var map = new List<Int64>();
+			while (output.Any())
 			{
-				var head = Positions.Dequeue();
+				map.Add(output.Dequeue());
+			}
+			Display(map);
 
-				for (int n = 1; n <= 4; ++n)
+			var mapCoords = new List<Coords>();
+
+			long x = 0;
+			long y = 0;
+
+			foreach (var l in map)
+			{
+				if (l == 10)
 				{
-					var newDrone = head.Move(n, Explored);
-					if (newDrone == null)
-						continue;
-					if (newDrone.AtTarget)
+					++y;
+					x = 0;
+					continue;
+				}
+				else
+				{
+					Map.Add(new Coords(x++, y, 0, (Tile)l));
+				}
+			}
+
+			long maxX = Map.Max(c => c.X);
+			long maxY = Map.Max(c => c.Y);
+
+			int sum = 0;
+
+			for (int j = 1; j < maxY - 1; ++j)
+			{
+				for (int i = 1; i < maxX - 1; ++i)
+				{
+					var current = Map.Where(c => c.X == i && c.Y == j).First();
+					if (current.Tile == Tile.Scaffold)
 					{
-						onTarget = newDrone;
-						return newDrone.Coordinates.Distance;
+						if (Map.Exists(c => (current.X == c.X - 1 && current.Y == c.Y && c.Tile == Tile.Scaffold))
+							&& Map.Exists(c => (current.X == c.X + 1 && current.Y == c.Y && c.Tile == Tile.Scaffold))
+							&& Map.Exists(c => (current.X == c.X && current.Y == c.Y - 1 && c.Tile == Tile.Scaffold))
+							&& Map.Exists(c => (current.X == c.X && current.Y == c.Y + 1 && c.Tile == Tile.Scaffold)))
+							sum += i * j;
 					}
-					Positions.Enqueue(newDrone);
 				}
 			}
-			return -1;
+			Console.WriteLine($"Part 1: {sum}");
 		}
 
-		public static int Part2(Int64[] input)
+		static void Part2(Int64[] input)
 		{
-			if (onTarget == null)
-				Part1(input);
-			return FillSpace(onTarget);
-		}
-		
-		// BFS from Oxygen System
-		public static int FillSpace(Drone drone)
-		{
-			Explored = new HashSet<(Int64, Int64)>();
-			drone.Coordinates = new Coords(drone.Coordinates.X, drone.Coordinates.Y, 0);
+			var M = new Int64[] { 'A', ',', 'B', ',', 'B', ',', 'A', ',', 'B', ',', 'C', ',', 'A', ',', 'C', ',', 'B', ',', 'C', '\n' };
+			var A = new Int64[] { 'L', ',', '4', ',', 'L', ',', '6', ',', 'L', ',', '8', ',', 'L', ',', '1', '2', '\n' };
+			var B = new Int64[] { 'L', ',', '8', ',', 'R', ',', '1', '2', ',', 'L', ',', '1', '2', '\n' };
+			var C = new Int64[] { 'R', ',', '1', '2', ',', 'L', ',', '6', ',', 'L', ',', '6', ',', 'L', ',', '8', '\n' };
+			var V = new Int64[] { 'n', 10 };
 
-			Positions = new Queue<Drone>();
-			Positions.Enqueue(drone);
+			input[0] = 2;
 
-			Drone head = null;
-			while (Positions.Count > 0)
+			var bot = new ASCIIRobot(input, null);
+
+			bot.Brain.Run();
+
+			var output = bot.Brain.GetOutputQueue();
+
+			var map = new List<Int64>();
+			while (output.Any())
 			{
-				head = Positions.Dequeue();
-
-				for (int n = 1; n <= 4; ++n)
-				{
-					var newDrone = head.Move(n, Explored);
-					if (newDrone == null)
-						continue;
-					Positions.Enqueue(newDrone);
-				}
-
+				map.Add(output.Dequeue());
 			}
-			return head.Coordinates.Distance;
+			Display(map);
+
+			foreach (var c in M)
+			{
+				bot.Brain.SendInput(c);
+				Console.Write((char)c);
+			}
+
+			bot.Brain.Run();
+
+			map = new List<Int64>();
+			while (output.Any())
+			{
+				map.Add(output.Dequeue());
+			}
+			Display(map);
+
+			foreach (var c in A)
+			{
+				bot.Brain.SendInput(c);
+				Console.Write((char)c);
+			}
+
+			bot.Brain.Run();
+
+			map = new List<Int64>();
+			while (output.Any())
+			{
+				map.Add(output.Dequeue());
+			}
+			Display(map);
+
+			foreach (var c in B)
+			{
+				bot.Brain.SendInput(c);
+				Console.Write((char)c);
+			}
+
+			bot.Brain.Run();
+
+			map = new List<Int64>();
+			while (output.Any())
+			{
+				map.Add(output.Dequeue());
+			}
+			Display(map);
+
+			foreach (var c in C)
+			{
+				bot.Brain.SendInput(c);
+				Console.Write((char)c);
+			}
+
+			bot.Brain.Run();
+
+			map = new List<Int64>();
+			while (output.Any())
+			{
+				map.Add(output.Dequeue());
+			}
+			Display(map);
+
+			foreach (var c in V)
+			{
+				bot.Brain.SendInput(c);
+				Console.Write((char)c);
+			}
+
+			bot.Brain.Run();
+
+			map = new List<Int64>();
+			while (output.Any())
+			{
+				map.Add(output.Dequeue());
+			}
+			Display(map);
+
+			bot.Brain.Run();
+
+			if (bot.Brain.GetOutputQueue().Any())
+			{
+				Console.WriteLine($"Part 2: {bot.Brain.GetOutputQueue().Dequeue()}");
+			}
+		}
+
+		static void Display(List<Int64> map)
+		{
+			foreach (var l in map)
+			{
+				if (l == 10)
+					Console.WriteLine();
+				else
+					Console.Write((char)l);
+			}
 		}
 	}
 }
