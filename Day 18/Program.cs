2cec466bca06b4bd0bbb2ece1fbe7e5150466540
Dennis Man <dman@questica.com>
2019-12-18 00:03:34 -0500

    Day 18 initial commit

---
+++ Day 18/Program.cs
@@ -0,0 +1,17 @@
+﻿using Shared_Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_18
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var input = Utilities.ReadFile(args[0]);
+		}
+	}
+}
